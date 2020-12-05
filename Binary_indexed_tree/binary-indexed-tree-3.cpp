5aa09d772bedfe12451a9e9a5c8a8786e6ea5f16
thptbadiem-tutor <thptbadiem.tutor@gmail.com>
2020-12-06 00:36:46 +0700

    Add solutions of BIT3 - Dec 06

---
+++ Binary_indexed_tree/binary-indexed-tree-3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+const int max_val = 2000001;
+int T[max_val + 1] = {0};
+
+void Update(int label, int val) {
+    while (label <= max_val) {
+        T[label] += val;
+        label += label & -label;
+    }
+}
+int Count(int label) {
+    int res = 0;
+    while (label) {
+        res += T[label];
+        label -= label & -label;
+    }
+    return res;
+}
+
+int main() {
+    int offset = 1000001;   
+    int n;
+    cin >> n;
+    for (int i = 0, a; i < n; ++ i) {
+        cin >> a;
+        Update(a + offset, 1);
+        cout << Count(a + offset - 1) << endl;
+    }
+    return 0;
+}
