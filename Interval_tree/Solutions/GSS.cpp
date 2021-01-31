626317bd06c6a3a355d39fe2f536acae152b314c
thptbadiem-tutor <thptbadiem.tutor@gmail.com>
2021-01-31 08:31:23 +0700

    Edit Interval_Tree/GSS solutions - Jan 31

--- Interval_tree/Solutions/GSS.cpp
+++ Interval_tree/Solutions/GSS.cpp
@@ -1,38 +1,41 @@
+/*
+ * Đề bài: https://codeforces.com/group/FLVn1Sc504/contest/274493/problem/B
+ */
 #include <iostream>
 #include <vector>
 
 using namespace std;
 const int NEGATIVE_INFINITY = -15000 * 50000 - 1;
 const int MAX_INT = 2147483647;
-struct Type {
-    int sum, max_sum, max_prefix, max_suffix;
+struct Data {
+    int sum, result, max_prefix, max_suffix;
     void Assign(int value) {
         this->sum = value;
-        this->max_sum = value;
+        this->result = value;
         this->max_prefix = value;
         this->max_suffix = value;
     }
 };
 
-Type Combine(Type left, Type right) {
-    Type res;
+Data Combine(Data left, Data right) {
+    Data res;
     res.sum = left.sum + right.sum;
     res.max_prefix = max(left.max_prefix, left.sum + right.max_prefix);
     res.max_suffix = max(left.max_suffix + right.sum, right.max_suffix);
-    res.max_sum = max(max(left.max_sum, right.max_sum), left.max_suffix + right.max_prefix);
+    res.result = max(max(left.result, right.result), left.max_suffix + right.max_prefix);
     return res;
 }
 
-Type operator+(Type left, Type right) {
-    Type res;
+Data operator+(Data left, Data right) {
+    Data res;
     res.sum = left.sum + right.sum;
     res.max_prefix = max(left.max_prefix, left.sum + right.max_prefix);
     res.max_suffix = max(left.max_suffix + right.sum, right.max_suffix);
-    res.max_sum = max(max(left.max_sum, right.max_sum), left.max_suffix + right.max_prefix);
+    res.result = max(max(left.result, right.result), left.max_suffix + right.max_prefix);
     return res;
 }
 
-void Update(vector<Type> &T, int id, int l, int r, int index, int value) {
+void Update(vector<Data> &T, int id, int l, int r, int index, int value) {
     if (index < l || r < index) {
         return;
     }
@@ -47,7 +50,7 @@ void Update(vector<Type> &T, int id, int l, int r, int index, int value) {
     T[id] = T[left_id] + T[right_id];
 }
 
-Type GSS(vector<Type> &T, int id, int l, int r, int x, int y) {
+Data GSS(vector<Data> &T, int id, int l, int r, int x, int y) {
     if (x <= l && r <= y) {
         return T[id];
     }
@@ -58,17 +61,14 @@ Type GSS(vector<Type> &T, int id, int l, int r, int x, int y) {
     if (x > m) {
         return GSS(T, id * 2 + 1, m + 1, r, x, y);
     }
-    /* return Combine( */
-    /*     GSS(T, id * 2, l, m, x, y), */
-    /*     GSS(T, id * 2 + 1, m + 1, r, x, y) */
-    /* ); */
+    /* return Combine(GSS(T, id * 2, l, m, x, y), GSS(T, id * 2 + 1, m + 1, r, x, y)); */
     return GSS(T, id * 2, l, m, x, y) + GSS(T, id * 2 + 1, m + 1, r, x, y);
 }
 
 int main() {
     int n, m;
     cin >> n;
-    vector<Type> T(n * 4 + 1);
+    vector<Data> T(n * 4 + 1);
     for (int i = 1, a; i <= n; ++ i) {
         cin >> a;
         Update(T, 1, 1, n, i, a);
@@ -76,7 +76,7 @@ int main() {
     cin >> m;
     for (int i = 1, x, y; i <= m; ++ i) {
         cin >> x >> y;
-        cout << GSS(T, 1, 1, n, x, y).max_sum << endl;
+        cout << GSS(T, 1, 1, n, x, y).result << endl;
     }
     return 0;
 }
